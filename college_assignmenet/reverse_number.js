2f9175c7447e3afd7044513c8b5228cb49ac38f0
Maitri Panchal <maitripanchal1408@gmail.com>
2025-01-08 19:49:52 +0530

    initial commit

--- college_assignmenet/reverse_number.js
+++
@@ -1,9 +0,0 @@
-var number = 1234;
-var rem = 0;
-var reverse = 0;
-while (number > 0) {
-	rem = number % 10;
-	number = parseInt(number / 10);
-	reverse = reverse * 10 + rem;
-}
-console.log('reverse number is :' + reverse);
