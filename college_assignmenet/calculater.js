933b4a371f0580b164f460dd385787c8286fadea
Maitri Panchal <maitripanchal1408@gmail.com>
2025-01-08 21:39:04 +0530

    initial commit

---
+++ college_assignmenet/calculater.js
@@ -0,0 +1,11 @@
+var number = 14;
+var number2 = 8;
+calculator(number, number2);
+function calculator(number, number2) {
+	console.log('addition : ', +number + number2);
+	console.log('subtraction : ', +number - number2);
+	console.log('multiplication  : ', +number * number2);
+	console.log('modulo : ', +(number % number2));
+	if (number == 0 || number2 == 0) console.log('Division : Error, Division by zero is not allowed');
+	else console.log('Division :', +number / number2);
+}
