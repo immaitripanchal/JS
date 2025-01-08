809fd7c591c9ede0065fccf83dd8da2653102783
Maitri Panchal <maitripanchal1408@gmail.com>
2025-01-08 21:52:48 +0530

    initial commit

--- college_assignmenet/max_array.js
+++
@@ -1,8 +0,0 @@
-// var arr = [1, 4, 2, 6, 8, 5, 1, 9, 53, 12, 8];
-// var max = arr[0];
-// for (var i = 0; i < arr.length; i++) {
-// 	if (arr[i] > max) {
-// 		max = arr[i];
-// 	}
-// }
-// console.log(max);
