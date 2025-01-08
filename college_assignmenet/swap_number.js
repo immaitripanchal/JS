ed7803f569ff6d5a5d202ab2136957441213ecfc
Maitri Panchal <maitripanchal1408@gmail.com>
2025-01-08 22:23:49 +0530

    initial commit

---
+++ college_assignmenet/swap_number.js
@@ -0,0 +1,23 @@
+var n = 2;
+var n1 = 5;
+console.log('number before swapping :', n + ' ' + n1);
+var temp;
+temp = n;
+n = n1;
+n1 = temp;
+console.log('number after swapping :', n + ' ' + n1);
+// <!DOCTYPE html>
+// <html lang="en">
+// 	<head> </head>
+// 	<body>
+// 		<script>
+// 			var n = prompt('Enter first number');
+// 			var n1 = prompt('Enter second number');
+// 			document.write('Number before swapping : ', n + '&nbsp;' + n1);
+// 			var temp = n;
+// 			n = n1;
+// 			n1 = temp;
+// 			document.write('<br>Number after swapping : ', n + '&nbsp;' + n1);
+// 		</script>
+// 	</body>
+// </html>
